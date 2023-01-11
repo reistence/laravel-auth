8dfb74bff781c8ee8709238560b10c45e0ed2c9f
reistence <gvlamberti@gmail.com>
2023-01-11 16:25:53 +0100

    welcome page design altered

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.app')
 @section('content')
 
-<div class="jumbotron p-5 mb-4 bg-danger text-white rounded-3">
+<div class="myjumbotron p-5 mb-4  text-white rounded-3">
+    <img src="{{Vite::asset('resources/img/wave.svg')}}" alt="" srcset="">
     <div class="container py-5">
     
         <h1 class="display-5 fw-bold">
@@ -12,9 +13,23 @@
     </div>
 </div>
 
-<div class="content">
+<div class="content mt-5">
     <div class="container bg-dark text-white">
         <p>Login in order to access all of its features!</p>
+           <div class="dashboard-icons">
+                            <div class="feature">
+                                <i class="fa-solid fa-folder-tree text-danger"></i>
+                                <span class="fs-1">Access all your Projects</span>
+                            </div>
+                            <div class="feature">
+                                <i class="fa-solid fa-database text-danger"></i>
+                                <span class="fs-1">Store them all in one place</span>
+                            </div>
+                             <div class="feature">
+                                <i class="fa-solid fa-file-pen text-danger"></i>
+                                <span class="fs-1">Save and edit instantly</span>
+                            </div>
+                        </div>
     </div>
 </div>
 @endsection
